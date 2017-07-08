0e2f5897343b1adf4c8cfa8ad121122a25d79522
John Garrett <john.garrett718@gmail.com>
2017-07-08 09:59:06 -0500

    Created categoryId to processAddCheeseForm controller

--- src/main/java/org/launchcode/controllers/CheeseController.java
+++ src/main/java/org/launchcode/controllers/CheeseController.java
@@ -1,5 +1,6 @@
 package org.launchcode.controllers;
 
+import org.launchcode.models.Category;
 import org.launchcode.models.Cheese;
 import org.launchcode.models.data.CategoryDao;
 import org.launchcode.models.data.CheeseDao;
@@ -46,13 +47,18 @@ public class CheeseController {
     }
 
     @RequestMapping(value = "add", method = RequestMethod.POST)
-    public String processAddCheeseForm(@ModelAttribute  @Valid Cheese newCheese,
-                                       Errors errors, Model model) {
+    public String processAddCheeseForm(@ModelAttribute @Valid Cheese newCheese,
+                                       Errors errors, @RequestParam int categoryId,
+                                       Model model) {
 
         if (errors.hasErrors()) {
             model.addAttribute("title", "Add Cheese");
             return "cheese/add";
         }
+
+        Category cat = categoryDao.findOne(categoryId);
+        newCheese.setCategory(cat);
+
 
         cheeseDao.save(newCheese);
         return "redirect:";
