95004e9e9301f3081585be4caf4e5c6635ba48c9
John Garrett <john.garrett718@gmail.com>
2017-07-09 07:13:19 -0500

    Added class Menu.java

--- src/main/java/org/launchcode/models/Menu.java
+++ src/main/java/org/launchcode/models/Menu.java
@@ -1,13 +1,18 @@
 package org.launchcode.models;
 
-import javax.persistence.*;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.ManyToMany;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
 import java.util.ArrayList;
 import java.util.List;
 
 @Entity
-public class Category {
+public class Menu {
+
     @Id
     @GeneratedValue
     private int id;
@@ -16,13 +21,21 @@ public class Category {
     @Size(min = 3, max = 15)
     private String name;
 
-    @OneToMany
-    @JoinColumn(name = "category_id")
+    @ManyToMany
     private List<Cheese> cheeses = new ArrayList<>();
 
+    public void additem(Cheese item) {
+    }
+
+    public Menu() {
+    }
 
     public int getId() {
         return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
     }
 
     public String getName() {
@@ -33,15 +46,5 @@ public class Category {
         this.name = name;
     }
 
-    public Category() {
-
-    }
-
-    public Category(String name) {
-        this.name = name;
-    }
-
 
 }
-
-
